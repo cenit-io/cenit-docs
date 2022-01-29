3f56a3e9bd5a7d7d0087fdb5d3e34349d6a8b50a
Miguel Sancho <sanchojaf@gmail.com>
2022-01-29 06:16:40 -0500

    Change logo and favicon

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,7 +8,7 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 const config = {
   title: 'Cenit IO - Docs',
   tagline: 'Open iPaaS',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://master.dh52xlqrqmf4x.amplifyapp.com',
   baseUrl: '/',
   onBrokenLinks: 'ignore', //'throw',
   onBrokenMarkdownLinks: 'warn',
